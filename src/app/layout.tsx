fb60bb08cb823abfebe410100def0c217b4e5b9b
Achitapon Jaturasuksakul <achitapon.jaturasuksakul@gmail.com>
2025-05-18 02:22:50 +0700

    sprash screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,49 +39,88 @@ export default function RootLayout({
         <link
           rel="apple-touch-startup-image"
           media="screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-          href="https://rukminim1.flixcart.com/www/1290/2792/promos/21/07/2024/dc4cedcb-4f40-4188-8655-52496286257d.jpg?q=100"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
         />
         <link
           rel="apple-touch-startup-image"
           media="screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-          href="https://rukminim1.flixcart.com/www/1179/2556/promos/19/07/2024/b7db38ec-bce9-47af-bc79-bc952ba0918d.png?q=100"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
         />
         <link
           rel="apple-touch-startup-image"
           media="screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-          href="https://rukminim1.flixcart.com/www/1170/2532/promos/18/07/2024/2ee2c044-13f0-4473-8200-9605f9afbd4d.png?q=100"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
         />
         <link
           rel="apple-touch-startup-image"
           media="screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-          href="https://rukminim1.flixcart.com/www/1284/2778/promos/21/07/2024/b2b12164-277f-4fbe-9afe-a86bdac6cd14.png?q=100"
-        />
-        {/* Apple Splash Screens: iPad */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/PWA/splash/apple-splash-2048-2732.png"
-          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
         />
         <link
           rel="apple-touch-startup-image"
-          href="/PWA/splash/apple-splash-1668-2388.png"
-          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+          media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
         />
         <link
           rel="apple-touch-startup-image"
-          href="/PWA/splash/apple-splash-1668-2224.png"
-          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+          media="screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
         />
         <link
           rel="apple-touch-startup-image"
-          href="/PWA/splash/apple-splash-1536-2048.png"
-          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+          media="screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
         />
-
-        {/* Android Splash: ใช้ manifest.json */}
-        {/* Android Chrome จะใช้ splash จาก manifest icons และ background_color */}
-
-        {/* Apple Touch Icon */}
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+        />
         <link rel="apple-touch-icon" href="/PWA/icons/icon-192x192.png" />
       </head>
       <body>
