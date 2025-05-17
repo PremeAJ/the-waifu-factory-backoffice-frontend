dc4b1a39e4342a1124358116e27bea46fe3d83c7
Achitapon Jaturasuksakul <achitapon.jaturasuksakul@gmail.com>
2025-05-18 01:59:14 +0700

    test sprash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,41 +17,66 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <title>Meow Som</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
         <meta name="theme-color" content="#ea9e3b" />
         <link rel="icon" href="/PWA/icons/icon-192x192.png" />
         <link rel="manifest" href="/PWA/manifest.json" />
-        <meta name="description" content="แดชบอร์ดจัดการระบบ POS สำหรับร้านค้า MeowSom" />
-        <meta name="keywords" content="MeowSom, POS, Back Office, SaaS, CRM, ERP, HRM" />
+        <meta
+          name="description"
+          content="แดชบอร์ดจัดการระบบ POS สำหรับร้านค้า MeowSom"
+        />
+        <meta
+          name="keywords"
+          content="MeowSom, POS, Back Office, SaaS, CRM, ERP, HRM"
+        />
         <meta name="robots" content="index, follow" />
 
         {/* Apple Splash Screens: iPhone */}
-        <link rel="apple-touch-startup-image" href="/PWA/splash/apple-splash-1290-2796.png"
-          media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3)" />
-        <link rel="apple-touch-startup-image" href="/PWA/splash/apple-splash-1179-2556.png"
-          media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)" />
-        <link rel="apple-touch-startup-image" href="/PWA/splash/apple-splash-1170-2532.png"
-          media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)" />
-        <link rel="apple-touch-startup-image" href="/PWA/splash/apple-splash-1125-2436.png"
-          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" />
-        <link rel="apple-touch-startup-image" href="/PWA/splash/apple-splash-1242-2688.png"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" />
-        <link rel="apple-touch-startup-image" href="/PWA/splash/apple-splash-828-1792.png"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" />
-        <link rel="apple-touch-startup-image" href="/PWA/splash/apple-splash-750-1334.png"
-          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" />
-        <link rel="apple-touch-startup-image" href="/PWA/splash/apple-splash-640-1136.png"
-          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" />
-
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+          href="https://rukminim1.flixcart.com/www/1290/2792/promos/21/07/2024/dc4cedcb-4f40-4188-8655-52496286257d.jpg?q=100"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+          href="https://rukminim1.flixcart.com/www/1179/2556/promos/19/07/2024/b7db38ec-bce9-47af-bc79-bc952ba0918d.png?q=100"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+          href="https://rukminim1.flixcart.com/www/1170/2532/promos/18/07/2024/2ee2c044-13f0-4473-8200-9605f9afbd4d.png?q=100"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+          href="https://rukminim1.flixcart.com/www/1284/2778/promos/21/07/2024/b2b12164-277f-4fbe-9afe-a86bdac6cd14.png?q=100"
+        />
         {/* Apple Splash Screens: iPad */}
-        <link rel="apple-touch-startup-image" href="/PWA/splash/apple-splash-2048-2732.png"
-          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" />
-        <link rel="apple-touch-startup-image" href="/PWA/splash/apple-splash-1668-2388.png"
-          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" />
-        <link rel="apple-touch-startup-image" href="/PWA/splash/apple-splash-1668-2224.png"
-          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" />
-        <link rel="apple-touch-startup-image" href="/PWA/splash/apple-splash-1536-2048.png"
-          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" />
+        <link
+          rel="apple-touch-startup-image"
+          href="/PWA/splash/apple-splash-2048-2732.png"
+          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/PWA/splash/apple-splash-1668-2388.png"
+          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/PWA/splash/apple-splash-1668-2224.png"
+          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/PWA/splash/apple-splash-1536-2048.png"
+          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+        />
 
         {/* Android Splash: ใช้ manifest.json */}
         {/* Android Chrome จะใช้ splash จาก manifest icons และ background_color */}
@@ -62,9 +87,7 @@ export default function RootLayout({
       <body>
         <NextTopLoader color="#5D87FF" />
         <CustomizerContextProvider>
-          <MyApp>
-            {children}
-          </MyApp>
+          <MyApp>{children}</MyApp>
         </CustomizerContextProvider>
       </body>
     </html>
