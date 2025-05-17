a3361d6aed30e96f1f6e6e3c46558cfbced53c09
Achitapon Jaturasuksakul <achitapon.jaturasuksakul@gmail.com>
2025-05-18 02:37:33 +0700

    local sprash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,27 +39,27 @@ export default function RootLayout({
         <link
           rel="apple-touch-startup-image"
           media="screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+          href="/PWA/splash/sprash_screen.png"
         />
         <link
           rel="apple-touch-startup-image"
           media="screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+          href="/PWA/splash/sprash_screen.png"
         />
         <link
           rel="apple-touch-startup-image"
           media="screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+          href="/PWA/splash/sprash_screen.png"
         />
         <link
           rel="apple-touch-startup-image"
           media="screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+          href="/PWA/splash/sprash_screen.png"
         />
         <link
           rel="apple-touch-startup-image"
           media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-          href="https://afsafklyxzzychxntdwu.supabase.co/storage/v1/object/public/assets/sprash_screen/sprash_screen.png"
+          href="/PWA/splash/sprash_screen.png"
         />
         <link
           rel="apple-touch-startup-image"
