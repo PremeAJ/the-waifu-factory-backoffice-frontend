3aed291e2fa0b3ad45346430ae361d38053f9b30
Achitapon Jaturasuksakul <achitapon.jaturasuksakul@gmail.com>
2025-05-19 09:50:39 +0700

    diabled PWA

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const withPWACfg = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "developer",
 });
 
 const nextConfig = {
