10cd65d1527d584f51a1912302c6e9d6219fc0b8
Achitapon Jaturasuksakul <achitapon.jaturasuksakul@gmail.com>
2025-05-20 15:51:35 +0700

    remove development

--- src/app/development/DevGuard.tsx
+++
@@ -1,6 +0,0 @@
-import Error404 from "../auth/error/404/page";
-
-export default function DevGuard({ children }: { children: React.ReactNode }) {
-   const isDevelopment = process.env.NODE_ENV === "development";
-    return (isDevelopment && children || <Error404 />);
-}
