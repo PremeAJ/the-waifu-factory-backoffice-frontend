177a5233436d6c7c364cdc52fcc4edb9c4176885
Achitapon Jaturasuksakul <achitapon.jaturasuksakul@gmail.com>
2025-05-27 00:00:21 +0700

    fix auth

--- src/app/dashboard/(Layout)/authGuard.tsx
+++ src/app/dashboard/(Layout)/authGuard.tsx
@@ -8,16 +8,15 @@ export default function AuthGuard({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const path = usePathname();
   const [isLogingIn, setIsLogingIn] = useState(false);
-  const { user } = useContext(UserContext);
+  const { user, loading } = useContext(UserContext);
+
   useEffect(() => {
-    const checkSession = async () => {
-      if (!user) {
-        router.replace("/dashboard/auth/login");
-      } else {
-        setIsLogingIn(true);
-      }
-    };
-    checkSession();
-  }, [path]);
+    if (!loading && !user) {
+      router.replace("/dashboard/auth/login");
+    } else {
+      setIsLogingIn(true);
+    }
+  }, [path, user, loading]);
+
   return <>{(isLogingIn && children) || <Loading />}</>;
 }
