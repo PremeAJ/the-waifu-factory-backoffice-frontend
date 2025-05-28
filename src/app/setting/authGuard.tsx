c9b593b050f8f6f35d3ed0a19640ce667419efe2
Achitapon Jaturasuksakul <achitapon.jaturasuksakul@gmail.com>
2025-05-28 21:28:32 +0700

    setting root

---
+++ src/app/setting/authGuard.tsx
@@ -0,0 +1,22 @@
+import { AuthContext } from "@/app/context/AuthContext";
+import { UserContext } from "@/app/context/UserContext";
+import Loading from "@/app/loading";
+import { usePathname, useRouter } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
+
+export default function AuthGuard({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const path = usePathname();
+  const [isLogingIn, setIsLogingIn] = useState(false);
+  const { user, loading } = useContext(UserContext);
+
+  useEffect(() => {
+    if (!loading && !user) {
+      router.replace("/dashboard/auth/login");
+    } else {
+      setIsLogingIn(true);
+    }
+  }, [path, user, loading]);
+
+  return <>{(isLogingIn && children) || <Loading />}</>;
+}
