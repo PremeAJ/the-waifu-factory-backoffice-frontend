978f2df420041e5f4e933b346536fbb891053186
Achitapon Jaturasuksakul <achitapon.jaturasuksakul@gmail.com>
2025-06-09 20:00:19 +0700

    refactor: Improve authentication logic in AuthGuard component for better user session handling

--- src/app/dashboard/(Layout)/authGuard.tsx
+++ src/app/dashboard/(Layout)/authGuard.tsx
@@ -1,21 +1,23 @@
 import Loading from "@/app/loading";
+import { AuthContext } from "@/context/AuthContext";
 import { UserContext } from "@/context/UserContext";
 import { usePathname, useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-  const path = usePathname();
-  const [isLogingIn, setIsLogingIn] = useState(false);
-  const { user, loading } = useContext(UserContext);
+  const pathname = usePathname();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const { user: authUser, isLoading: authLoading } = useContext(AuthContext);
+  const { user: appUser, loading: appUserLoading } = useContext(UserContext);
 
   useEffect(() => {
-    if (!loading && !user) {
+    if (!authLoading && !authUser && !appUserLoading && !appUser) {
       router.replace("/dashboard/auth/login");
     } else {
-      setIsLogingIn(true);
+      setIsAuthenticated(true);
     }
-  }, [path, user, loading]);
+  }, [pathname, appUser, appUserLoading, authUser, authLoading]);
 
-  return <>{(isLogingIn && children) || <Loading />}</>;
+  return <>{(isAuthenticated && children) || <Loading />}</>;
 }
