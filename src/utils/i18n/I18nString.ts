cd0c8f7e0ae551a85695ff281dc7437e9c7a08c5
Achitapon Jaturasuksakul <achitapon.jaturasuksakul@gmail.com>
2025-06-17 11:55:44 +0700

    Refactor I18nString function to improve parameter handling and default values
    - Removed dependency on config for language setting.
    - Updated parameters to allow null values for stringTh and stringEn.
    - Ensured the function returns an empty string if both translations are not provided.

--- src/utils/i18n/I18nString.ts
+++ src/utils/i18n/I18nString.ts
@@ -1,5 +1,4 @@
-import config from "@/context/setting/config";
-
-export function I18nString(isLanguage = config.isLanguage, stringTh = '', stringEn = ''): string {
-  return isLanguage === "en" ? stringEn || stringTh : stringTh;
+export function I18nString(isLanguage: string, stringTh: string | null = "", stringEn: string | null = ""): string {
+  const text = isLanguage === "en" ? stringEn || stringTh : stringTh
+  return text || "";
 }
