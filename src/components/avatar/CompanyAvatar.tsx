aa3f28e8187cb8e09b9d61b322da087a37fb7606
Achitapon Jaturasuksakul <achitapon.jaturasuksakul@gmail.com>
2025-06-18 00:39:52 +0700

    feat: expand icon set in CompanyAvatar component for additional business types

--- src/components/avatar/CompanyAvatar.tsx
+++ src/components/avatar/CompanyAvatar.tsx
@@ -1,5 +1,26 @@
 import Avatar from "@mui/material/Avatar";
-import { IconBuildingCommunity, IconToolsKitchen2, IconCoffee, IconBaguette, IconBubbleTea2 } from "@tabler/icons-react";
+import {
+  IconBuildingCommunity,
+  IconToolsKitchen2,
+  IconCoffee,
+  IconBaguette,
+  IconBubbleTea2,
+  IconShoppingCart,
+  IconBasket,
+  IconBooks,
+  IconDeviceMobile,
+  IconPillFilled,
+  IconWashMachine,
+  IconEyeClosed,
+  IconDeviceTabletCog,
+  IconDeviceAirpods,
+  IconPaperclip,
+  IconMassage,
+  IconSparkles,
+  IconShirt,
+  IconShoe,
+  IconLuggage,
+} from "@tabler/icons-react";
 
 type CompanyAvatarProps = {
   businessTypeId?: number;
@@ -9,14 +30,28 @@ type CompanyAvatarProps = {
 
 const iconSize = {
   width: 24,
-  height: 24
-}
+  height: 24,
+};
 
 const iconMap: Record<number, React.ReactNode> = {
   1: <IconToolsKitchen2 {...iconSize} />,
   2: <IconCoffee {...iconSize} />,
   3: <IconBaguette {...iconSize} />,
   4: <IconBubbleTea2 {...iconSize} />,
+  5: <IconShoppingCart {...iconSize} />,
+  6: <IconBasket {...iconSize} />,
+  7: <IconBooks {...iconSize} />,
+  8: <IconDeviceTabletCog {...iconSize} />,
+  9: <IconPillFilled {...iconSize} />,
+  10: <IconWashMachine {...iconSize} />,
+  11: <IconEyeClosed {...iconSize} />,
+  12: <IconDeviceAirpods {...iconSize} />,
+  13: <IconPaperclip {...iconSize} />,
+  14: <IconSparkles {...iconSize} />,
+  15: <IconMassage {...iconSize} />,
+  16: <IconShirt {...iconSize} />,
+  17: <IconShoe {...iconSize} />,
+  18: <IconLuggage {...iconSize} />,
 };
 
 const CompanyAvatar = ({ businessTypeId, imageUrl, size = 40 }: CompanyAvatarProps) => (
