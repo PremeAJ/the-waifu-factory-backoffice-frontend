9781551fa4c4babf1a4049f8e7daa08e03f79217
Achitapon Jaturasuksakul <164000730+PaRaReem@users.noreply.github.com>
2025-07-30 16:35:27 +0700

    chore: No code changes detected; skipping commit.

--- src/app/dashboard/(company-auth)/(dashboard)/layout/header/Header.tsx
+++ src/app/dashboard/(company-auth)/(dashboard)/layout/header/Header.tsx
@@ -1,24 +1,17 @@
 import { ProductProvider } from "@/context/Ecommercecontext/index";
-import config from "@/context/setting/config";
-import { CustomizerContext } from "@/context/setting/customizerContext";
 import { UserContext } from "@/context/UserContext";
-import AppBar from "@mui/material/AppBar";
-import IconButton from "@mui/material/IconButton";
 import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
 import Toolbar from "@mui/material/Toolbar";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import { IconMenu2 } from "@tabler/icons-react";
 import { useContext } from "react";
-import Language from "../../../../../../components/shared/Language/Language";
-import Profile from "../../../../../../components/shared/Profile";
 import Cart from "./Cart";
 import MobileRightSidebar from "./MobileRightSidebar";
-import Navigation from "./Navigation";
 import Notifications from "./Notification";
-import Search from "./Search";
 import { Box, Typography } from "@mui/material";
 import AppBarStyled from "@/components/styled/AppBarStyled";
+import Language from "@/components/shared/Language/Language";
+import Profile from "@/components/shared/Profile";
 
 const Header = () => {
   const { loading, user } = useContext(UserContext);
@@ -26,10 +19,7 @@ const Header = () => {
   const { companyUsers } = companies || {};
   const { roles } = companyUsers?.[0] || {};
   const { nameTh: roleNameTh, nameEn: roleNameEN } = roles || {};
-  const lgUp = useMediaQuery((theme) => theme.breakpoints.up("lg"));
   const lgDown = useMediaQuery((theme) => theme.breakpoints.down("lg"));
-  const TopbarHeight = config.topbarHeight;
-  const { setIsCollapse, isCollapse, isMobileSidebar, setIsMobileSidebar } = useContext(CustomizerContext);
   const ToolbarStyled = styled(Toolbar)(({ theme }) => ({
     width: "100%",
     color: theme.palette.text.secondary,
@@ -39,33 +29,12 @@ const Header = () => {
     <ProductProvider>
       <AppBarStyled position="sticky" color="default">
         <ToolbarStyled>
-          <IconButton
-            color="inherit"
-            aria-label="menu"
-            onClick={() => {
-              if (lgUp) {
-                isCollapse === "full-sidebar" ? setIsCollapse("mini-sidebar") : setIsCollapse("full-sidebar");
-              } else {
-                setIsMobileSidebar(!isMobileSidebar);
-              }
-            }}
-          >
-            <IconMenu2 size="20" />
-          </IconButton>
-
-          <Search />
-          {lgUp ? (
-            <>
-              <Navigation />
-            </>
-          ) : null}
-
           <Box flexGrow={1} />
           <Stack spacing={1} direction="row" alignItems="center">
             <Language />
             <Cart />
             <Notifications />
-           <MobileRightSidebar /> 
+            <MobileRightSidebar />
             {!lgDown && (
               <Box display="flex" flexDirection="column" alignItems="flex-end" mr={1} sx={{ cursor: "default" }}>
                 <Typography variant="subtitle1" fontWeight="bold" noWrap>
