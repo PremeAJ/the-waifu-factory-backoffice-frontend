ee2769f0e951cfe67db0489534b07d435862daf9
Achitapon Jaturasuksakul <achitapon.jaturasuksakul@gmail.com>
2025-08-03 11:23:13 +0700

    fix: Refactor ActionButton component for improved mobile handling and code clarity

--- src/common/components/floating/ActionButton.tsx
+++ src/common/components/floating/ActionButton.tsx
@@ -4,20 +4,20 @@ import { IconArrowLeft, IconMenu2 } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
 import BaseFab from "../base/BaseFab";
 import { useCustomize } from "@/common/contexts/setting/customizerContext";
+import useIsMobile from "@/common/utils/breakpoints/isMobile";
 
 const hideButton = ["/", "/auth/callback", "/dashboard/auth/callback", "/dashboard/auth/login", "/auth/login"];
-const manualBackPrefixes = ["/auth","/dashboard/auth", "/dashboard/setting"];
+const manualBackPrefixes = ["/auth", "/dashboard/auth", "/dashboard/setting"];
 
 const ActionButton = () => {
+  const isMobie = useIsMobile();
   const router = useRouter();
   const pathname = usePathname();
-  const allowBack =
-    pathname.split("/").length > 3 ||
-    manualBackPrefixes.some((prefix) => pathname.startsWith(prefix));
+  const allowBack = pathname.split("/").length > 3 || manualBackPrefixes.some((prefix) => pathname.startsWith(prefix));
 
   const [show, setShow] = useState(false);
   const [visible, setVisible] = useState(false);
-  const { isMobileSidebar, setIsMobileSidebar } = useCustomize();
+  const { isMobileSidebar, setIsMobileSidebar, isCollapse, setIsCollapse } = useCustomize();
 
   useEffect(() => {
     if (!hideButton.includes(pathname)) {
@@ -29,6 +29,13 @@ const ActionButton = () => {
   }, [pathname]);
 
   const handleExited = () => setShow(false);
+  const sidebarAction = () => {
+    return isMobie
+      ? setIsMobileSidebar(!isMobileSidebar)
+      : isCollapse === "full-sidebar"
+      ? setIsCollapse("mini-sidebar")
+      : setIsCollapse("full-sidebar");
+  };
 
   if (!show) return null;
 
@@ -36,9 +43,7 @@ const ActionButton = () => {
     <BaseFab
       fadeDirection="left"
       sx={{ position: "fixed", top: 16, left: 16, zIndex: 100 }}
-      onClick={() =>
-        allowBack ? router.back() : setIsMobileSidebar(!isMobileSidebar)
-      }
+      onClick={() => (allowBack ? router.back() : sidebarAction())}
       aria-label="action"
       open={visible}
       onExited={handleExited}
