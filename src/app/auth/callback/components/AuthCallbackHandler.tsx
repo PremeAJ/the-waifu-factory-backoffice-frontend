35f950f3dad762527ee8ac461c4946804f73535c
Achitapon Jaturasuksakul <achitapon.jaturasuksakul@gmail.com>
2025-09-18 16:01:06 +0700

    fix: remove unused import and debug logs in AuthCallbackHandler

--- src/app/auth/callback/components/AuthCallbackHandler.tsx
+++ src/app/auth/callback/components/AuthCallbackHandler.tsx
@@ -4,7 +4,6 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import Loading from "@/app/loading";
-import { stat } from "fs";
 
 export default function AuthCallbackHandler() {
   console.log("AuthCallbackHandler -----");
@@ -12,13 +11,10 @@ export default function AuthCallbackHandler() {
   const { data: session, status } = useSession();
   
   useEffect(() => {
-    console.log("🚀 ~ AuthCallbackHandler ~ session:", session)
-    console.log("🚀 ~ AuthCallbackHandler ~ status:", status)
     if (status === "loading") return;
     if (session && status === "authenticated") {
       router.push(PageUrl.DASHBOARD);
     } else {
-      console.log('>>>>>>>>')
       router.replace(PageUrl.AUTH_SIGN_IN);
     }
   }, [session, status]);
