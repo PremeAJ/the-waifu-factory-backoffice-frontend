fd7d84a2f4509b644127c62347e96d98b90fff11
Achitapon <achitapon.preme@gmail.com>
2026-05-22 18:25:02 +0700

    fix: update text in AdoptableShowcase for clarity

--- src/components/landingpage/adoptable-showcase/AdoptableShowcase.tsx
+++ src/components/landingpage/adoptable-showcase/AdoptableShowcase.tsx
@@ -290,7 +290,7 @@ const AdoptableShowcase = () => {
               Adoptables 
             </Typography>
             <Typography variant="body1" color="text.secondary" mt={0.5}>
-              All adoptables are hand-drawn by our artists. Click on any card to learn more about the adoptable
+              All adoptables are hand-drawn by our artists. Click on any card to see detail
             </Typography>
             <Typography fontSize={10} color="info" mt={0.5}>
               (in this demo i showing closed adoptables too because i don't have many open adoptables in demo data, but in real app you will only see open adoptables here)
